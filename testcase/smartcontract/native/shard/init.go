ba0fa4b726d81ebbb2cf776b7bbebecb583b9134
qiluge <wangcheng@onchain.com>
2019-07-09 10:24:08 +0800

    reformat tset case

--- testcase/smartcontract/native/shard/init.go
+++ testcase/smartcontract/native/shard/init.go
@@ -15,8 +15,6 @@ func TestShardMgmtContract() {
 	testframework.TFramework.RegTestCase("NotifyParentCommitDpos", TestNotifyRootCommitDpos)
 	testframework.TFramework.RegTestCase("NotifyShardRootCommitDpos", TestNotifyShardCommitDpos)
 	testframework.TFramework.RegTestCase("ShardRetryCommitDpos", TestShardRetryCommitDpos)
-	testframework.TFramework.RegTestCase("GetShardCommitDposInfo", TestGetShardCommitDposInfo)
-	testframework.TFramework.RegTestCase("GetShardDetail", TestGetShardDetail)
 	testframework.TFramework.RegTestCase("UpdateShardConfig", TestUpdateShardConfig)
 
 	testframework.TFramework.RegTestCase("ShardAssetInit", TestAssetInit)
@@ -24,10 +22,6 @@ func TestShardMgmtContract() {
 	testframework.TFramework.RegTestCase("XShardTransferOng", TestXShardTransferOng)
 	testframework.TFramework.RegTestCase("XShardTransferOngRetry", TestXShardTransferOngRetry)
 	testframework.TFramework.RegTestCase("XShardTransferOep4Retry", TestXShardTransferOep4Retry)
-	testframework.TFramework.RegTestCase("ShardGetPendingTransfer", TestGetPendingTransfer)
-	testframework.TFramework.RegTestCase("ShardGetTransferDetail", TestGetTransferDetail)
-	testframework.TFramework.RegTestCase("ShardGetSupplyInfo", TestGetSupplyInfo)
-	testframework.TFramework.RegTestCase("ShardGetOep4Balance", TestGetOep4Balance)
 
 	testframework.TFramework.RegTestCase("ChangeContractMetaData", TestChangeMetaData)
 
@@ -40,6 +34,13 @@ func TestShardMgmtContract() {
 	testframework.TFramework.RegTestCase("ShardUserWithdrawFee", TestShardUserWithdrawFee)
 	testframework.TFramework.RegTestCase("ShardAddInitPos", TestAddInitPos)
 	testframework.TFramework.RegTestCase("ShardReduceInitPos", TestReduceInitPos)
+
+	testframework.TFramework.RegTestCase("ShardGetPendingTransfer", TestGetPendingTransfer)
+	testframework.TFramework.RegTestCase("ShardGetTransferDetail", TestGetTransferDetail)
+	testframework.TFramework.RegTestCase("ShardGetSupplyInfo", TestGetSupplyInfo)
+	testframework.TFramework.RegTestCase("ShardGetOep4Balance", TestGetOep4Balance)
+	testframework.TFramework.RegTestCase("GetShardCommitDposInfo", TestGetShardCommitDposInfo)
+	testframework.TFramework.RegTestCase("GetShardDetail", TestGetShardDetail)
 	testframework.TFramework.RegTestCase("ShardQueryView", TestGetShardView)
 	testframework.TFramework.RegTestCase("ShardQueryPeerInfo", TestGetShardPeerInfo)
 	testframework.TFramework.RegTestCase("ShardQueryUserInfo", TestGetShardUserInfo)
